c5e44f63bf665cf7ad3f5708fc27df927cab8e9d
kishanhitk <kishansharma1231@gmail.com>
2020-05-29 18:43:09 +0530

    Added signout route.

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 require("dotenv").config();
-
+const authRoutes = require("./routes/auth");
+//Making connection to DB
 mongoose
   .connect(process.env.DATABASE, {
     useNewUrlParser: true,
@@ -14,10 +15,19 @@ mongoose
   })
   .then(() => console.log("DB CONNECTED"))
   .catch((e) => console.log(`ERROR ${e}`));
+
+//Middlewares
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(cors());
+
+//Routes
+app.use("/api", authRoutes);
+
+//Port
 const port = 4000;
+
+//Starting the server
 app.listen(port, console.log(`App is running on ${port}`));
 app.get("/", function (req, res) {
   res.send("Welcome");
