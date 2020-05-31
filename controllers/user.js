f41866c3c218bf95a301d939e9e6bf3ae12979d2
kishanhitk <kishansharma1231@gmail.com>
2020-05-31 14:16:16 +0530

    Added user purchase list

--- controllers/user.js
+++ controllers/user.js
@@ -1,4 +1,5 @@
 const User = require("../models/user");
+const Order = require("../models/order");
 
 exports.getUserById = (req, res, next, id) => {
   User.findById(id).exec((err, user) => {
@@ -39,4 +40,19 @@ exports.updateUser = (req, res) => {
       res.json(req.profile);
     }
   );
+};
+
+exports.userPurchaseList = (req, res) => {
+  Order.find({ user: req.profile._id })
+    .poulate("user", "_id name")
+    .exec((err, order) => {
+      if (err) {
+        return res.status(400).json({
+          err: "No orders found",
+        });
+      }
+      res.json({
+        order,
+      });
+    });
 };
