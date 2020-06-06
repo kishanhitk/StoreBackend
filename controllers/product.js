dd466c23cc4749bc2ef68fad3ef0c174ede7acbc
kishanhitk <kishansharma1231@gmail.com>
2020-06-06 20:52:54 +0530

    Adde create product from formidable.

--- controllers/product.js
+++ controllers/product.js
@@ -1,4 +1,7 @@
 const Product = require("../models/product");
+const formidale = require("formidable");
+const _ = require("lodash");
+const fs = require("fs");
 
 exports.getProductById = (req, res, id, next) => {
   Product.findById(id)
@@ -15,5 +18,27 @@ exports.getAllProducts = (req, res) => {
     if (err || !products)
       return res.status(400).json({ err: "No Products Found" });
     res.json(products);
+  });
+};
+
+exports.createProduct = (req, res) => {
+  let form = new formidale.IncomingForm();
+  form.keepExtensions = true;
+
+  form.parse(req, (err, fields, file) => {
+    if (err) return res.status(400).json("Saving to DB failed");
+
+    let product = new Product(fields);
+    if (file.photo) {
+      if (file.photo.size > 300000) {
+        return res.status(400).json({ err: "File Size too big" });
+      }
+      product.photo.data = fs.readFileSync(file.photo.path);
+      product.photo.contentType = file.photo.type;
+    }
+    product.save((err, product) => {
+      if (err) return res.status(400).json({ err: "Saving to DB Failed" });
+      res.json(product);
+    });
   });
 };
--- routes/product.js
+++ routes/product.js
@@ -1,12 +1,24 @@
 const express = require("express");
 const router = express.Router();
 
-const { getProductById, getAllProducts } = require("../controllers/product");
+const {
+  getProductById,
+  getAllProducts,
+  createProduct,
+} = require("../controllers/product");
 const { getUserById } = require("../controllers/user");
 const { isSignedIn, isAdmin, isAuthenticated } = require("../controllers/auth");
 
 router.param("/productId", getProductById);
 router.param("/userId", getUserById);
+
+router.post(
+  "/product/create/:userId",
+  isSignedIn,
+  isAuthenticated,
+  isAdmin,
+  createProduct
+);
 
 router.get("/products", getAllProducts);
 
