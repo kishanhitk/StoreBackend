55a312b915a59b38609cbff8222d67d38fb6f707
kishanhitk <kishansharma1231@gmail.com>
2020-06-06 18:30:23 +0530

    Added create category routes

--- controllers/category.js
+++ controllers/category.js
@@ -1,4 +1,5 @@
 const Category = require("../models/category");
+const category = require("../models/category");
 
 exports.getCategoryById = (req, res, next, id) => {
   Category.findById(id).exex((err, cate) => {
@@ -8,5 +9,16 @@ exports.getCategoryById = (req, res, next, id) => {
       });
     req.category = cate;
     next();
+  });
+};
+
+exports.createCategory = (req, res) => {
+  const category = new Category(req.body);
+  category.save((err, category) => {
+    if (err)
+      res.status(400).json({
+        err: "Could not save category to DB",
+      });
+    res.json({ category });
   });
 };
--- routes/category.js
+++ routes/category.js
@@ -3,9 +3,17 @@ const router = express.Router();
 
 const { isSignedIn, isAdmin, isAuthenticated } = require("../controllers/auth");
 const { getUserById } = require("../controllers/user");
-const { getCategoryById } = require("../controllers/category");
+const { getCategoryById, createCategory } = require("../controllers/category");
 
 router.param("userId", getUserById);
 router.param("categoryId", getCategoryById);
+
+router.post(
+  "category/create/:usedID",
+  isSignedIn,
+  isAuthenticated,
+  isAdmin,
+  createCategory
+);
 
 module.exports = router;
